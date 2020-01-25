f3e7416872eab1cebac018c9fec23f373e997af0
luis carlos <luisestrela.bol951@gmail.com>
2020-01-24 22:06:31 -0300

    heroku test deploy

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const routes = require('./routes');
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+})
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
